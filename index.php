98cff7b72930cc819f5886a5b4659abb49ecb393
Pauline-hiez <pauline.hiez@laplateforme.io>
2025-09-30 13:23:24 +0200

    index

--- index.php
+++ index.php
@@ -4,11 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/style.css">
     <title>Accueil</title>
 </head>
 <body>
 
-<header>
+<header class="bg-dark text-white py-4">
 <div class="d-flex justify-content-sm-center"><h1>Bienvenue sur mon site</h1></div>
     
 
@@ -22,7 +23,7 @@
     </button>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="index.php">Accueil</a>
         </li>
@@ -44,10 +45,66 @@
 </nav>
 
     
-<main></main>
+<main>
+
+<div class="container text-center my-5">
+  
+  <div class="row g-4">
+    <div class="col-md-4">
+      <div class="d-flex flex-column align-items-center">
+        <img src="assets/img/Thailand.jpg" class="img-fluid custom-img mb-3" alt="Image 1">
+        <button class="btn text-white" style="background-color:#052526;">Thailand</button>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="d-flex flex-column align-items-center">
+        <img src="assets/img/Grece.jpg" class="img-fluid custom-img mb-3" alt="Image 2">
+        <button class="btn text-white" style="background-color:#052526;">Grèce</button>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="d-flex flex-column align-items-center">
+        <img src="assets/img/Caledonie.jpg" class="img-fluid custom-img mb-3" alt="Image 3">
+        <button class="btn text-white" style="background-color:#052526;">Nouvelle-Calédonie</button>
+      </div>
+    </div>
+  </div>
+
+  <div class="row g-4 mt-0">
+    <div class="col-md-4">
+      <div class="d-flex flex-column align-items-center">
+        <img src="assets/img/Espagne.jpg" class="img-fluid custom-img mb-3" alt="Image 4">
+        <button class="btn text-white" style="background-color:#052526;">Espagne</button>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="d-flex flex-column align-items-center">
+        <img src="assets/img/Bretagne.jpg" class="img-fluid custom-img mb-3" alt="Image 5">
+        <button class="btn text-white" style="background-color:#052526;">Bretagne</button>
+      </div>
+    </div>
+    <div class="col-md-4">
+      <div class="d-flex flex-column align-items-center">
+        <img src="assets/img/Chambord.jpg" class="img-fluid custom-img mb-3" alt="Image 6">
+        <button class="btn text-white" style="background-color:#052526;">Château de Chambord</button>
+      </div>
+    </div>
+  </div>
+
+  <div class="row g-4 mt-0">
+    <div class="col-md-4 offset-md-4">
+      <div class="d-flex flex-column align-items-center">
+        <img src="assets/img/Villandry.jpg" class="img-fluid custom-img mb-3" alt="Image 7">
+        <button class="btn text-white" style="background-color:#052526;">Château de Villandry</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+</main>
 
 
-<footer>
+<footer class="bg-dark text-white py-4">
 
     <h5 class="d-flex justify-content-start gap-3 ms-3">Nous suivre :</h5>
     <div class="d-flex justify-content-start gap-3 ms-3">
